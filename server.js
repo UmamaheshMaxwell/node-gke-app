d1fe4b284c246d4d05f8063ea330408ca1f12d37
UmamaheshMaxwell <maheshmeka16@gmail.com>
2023-05-14 14:39:28 +0530

    modified server file

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ app.get("/", (req, res) => {
     res.json("Welcome to NodeApp deployed to GKE")
 })
 
-app.get("/user", (req, res)=> {
-    res.json([{
-        id: 1, name: "Scott Desatnick",
-        id: 2, name: "Adam Colson",
-        id: 3, name: "Tuan Bui"
-    }])
+app.get("/user", (req, res) => {
+    res.json([
+        { id: 1, name: "Scott Desatnick" },
+        { id: 2, name: "Adam Colson" },
+        { id: 3, name: "Tuan Bui" }
+    ])
 })
 
 const PORT = 8080
